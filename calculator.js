c0c8ff0b61eb554557b1bb9697debb61062d0df3
AhmedAnwar616 <ahmedanwar111994@gmail.com>
2024-10-10 16:17:49 +0300

    simple edit

--- calculator.js
+++ calculator.js
@@ -2,11 +2,12 @@
 const add = (number1, number2) => {
     return number1 + number2;
 }
-const subtraction = () => {
+const addition = () => {
 number1 = parseFloat(document.querySelector('number1').value) || 0;
 number2 = parseFloat(document.querySelector('number2').value) || 0;
 result = add (number1, number2);
 document.querySelector('calculation-result').textContent = result;
 };
 
-document.querySelector('add').addEventListener('click', subtraction);
+document.querySelector('add').addEventListener('click', addition);
+
