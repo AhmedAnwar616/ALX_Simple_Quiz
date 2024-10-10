962add420bd43c02cdb63d36683464062dd1fbe3
AhmedAnwar616 <ahmedanwar111994@gmail.com>
2024-10-10 06:10:07 +0300

    0. Implement a Simple Interactive Quiz

---
+++ Fundamentals of JavaScript/quiz.js
@@ -0,0 +1,15 @@
+// Function Declaration
+
+const checkAnswer = () => {
+    correctAnswer = '4';  // Correct Answer      
+    userAnswer = document.querySelector(input [type="radio"]).getattribute('value'); //Retrieve the User’s Answer
+    if (userAnswer == correctAnswer ) {
+        document.querySelector('feedback').textContent = 'Correct! Well done.';
+    } else {
+        document.querySelector('feedback').textContent = "That's incorrect. Try again!";
+    }
+};
+
+// Add an Event Listener to the Submit Button
+
+document.getElementById("submit-answer").addEventListener('click' , 'checkAnswer');
