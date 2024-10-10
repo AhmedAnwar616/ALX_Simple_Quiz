f2800a9a89d80e000561c3e2336f9f2e2ddb7ae9
AhmedAnwar616 <ahmedanwar111994@gmail.com>
2024-10-10 16:29:50 +0300

    simple

--- calculator.js
+++ calculator.js
@@ -3,52 +3,52 @@ const add = (number1, number2) => {
     return number1 + number2;
 }
 const addition = () => {
-number1 = parseFloat(document.querySelector('number1').value) || 0;
-number2 = parseFloat(document.querySelector('number2').value) || 0;
+number1 = parseFloat(document.getElementById('number1').value) || 0;
+number2 = parseFloat(document.getElementById('number2').value) || 0;
 result = add (number1, number2);
-document.querySelector('calculation-result').textContent = result;
+document.getElementById('calculation-result').textContent = result;
 };
 
-document.querySelector('add').addEventListener('click', addition);
+document.getElementById('add').addEventListener('click', addition);
 
 // subtract
 const subtract = (number1, number2) => {
     return number1 - number2;
 }
 const subtraction = () => {
-number1 = parseFloat(document.querySelector('number1').value) || 0;
-number2 = parseFloat(document.querySelector('number2').value) || 0;
+number1 = parseFloat(document.getElementById('number1').value) || 0;
+number2 = parseFloat(document.getElementById('number2').value) || 0;
 result = subtract (number1, number2);
-document.querySelector('calculation-result').textContent = result;
+document.getElementById('calculation-result').textContent = result;
 };
 
-document.querySelector('subtract').addEventListener('click', subtraction);
+document.getElementById('subtract').addEventListener('click', subtraction);
 
 // multiplie
 const  multiply = (number1, number2) => {
     return number1 * number2;
 }
 const multiplication = () => {
-number1 = parseFloat(document.querySelector('number1').value) || 0;
-number2 = parseFloat(document.querySelector('number2').value) || 0;
+number1 = parseFloat(document.getElementById('number1').value) || 0;
+number2 = parseFloat(document.getElementById('number2').value) || 0;
 result = multiply (number1, number2);
-document.querySelector('calculation-result').textContent = result;
+document.getElementById('calculation-result').textContent = result;
 };
 
-document.querySelector('multiply').addEventListener('click', multiplication);
+document.getElementById('multiply').addEventListener('click', multiplication);
 
 // division
 const  divide = (number1, number2) => {
     return number1 / number2;
 }
 const division = () => {
-number1 = parseFloat(document.querySelector('number1').value) || 0;
-number2 = parseFloat(document.querySelector('number2').value) || 0;
+number1 = parseFloat(document.getElementById('number1').value) || 0;
+number2 = parseFloat(document.getElementById('number2').value) || 0;
 result = divide (number1, number2);
-document.querySelector('calculation-result').textContent = result;
+document.getElementById('calculation-result').textContent = result;
 };
 
-document.querySelector('divide').addEventListener('click', division);
+document.getElementById('divide').addEventListener('click', division);
 
 
 
